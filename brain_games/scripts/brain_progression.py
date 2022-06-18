4e7d48f162e4fb3967cd18e50c3dc4747b5b4800
Яна Гаранина <yana.tarasenko9@yandex.ru>
2022-06-18 14:25:07 +0300

    add Makefile

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from brain_games.games import progression
+from brain_games.engine import engine
+
+def main():
+    engine(progression)
+
+
+if __name__ == '__main__':
+    main()
